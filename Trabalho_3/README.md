2fa3370e66e57739c8f46e1fcde29cf42e145759
GuilhermeChaves <guilherme.chaves@inf.ufrgs.br>
2022-03-28 21:10:13 -0300

    Qestao de gradiente pronta

---
+++ Trabalho_3/README.md
@@ -0,0 +1,26 @@
+Gabriel Lima Chimifosk - 00134078 - Turma B \
+Guilherme Sonaglio Chaves - 00285686 - Turma B \
+Tiago de Carvalho Magnus - 00287710 - Turma B
+
+As bibliotecas utilizadas são comuns ao ambiente python: random, copy e numpy.
+
+# Bibilotecas instaladas
+
+
+# Valores dos parâmetros do algoritmo genético
+g:
+n:
+k:
+m:
+e:
+
+
+# Valores utilizados na regressão linear
+theta_0 inicial: -3.2
+theta_1 inicial: 1
+alpha: 0.01
+num_iterations: 400
+Melhor EQM obtido: 8.528419301376648
+
+
+# Bibliografia
--- Trabalho_3/alegrete.py
+++ Trabalho_3/alegrete.py
@@ -1,42 +1,55 @@
-import numpy as np
-
-
 def compute_mse(theta_0, theta_1, data):
-    """
-    Calcula o erro quadratico medio
-    :param theta_0: float - intercepto da reta
-    :param theta_1: float -inclinacao da reta
-    :param data: np.array - matriz com o conjunto de dados, x na coluna 0 e y na coluna 1
-    :return: float - o erro quadratico medio
-    """
-    raise NotImplementedError  # substituir pelo seu codigo
+    mse = 0
+    num_linhas = 0
 
+    for line in data:
+        x = line[0]
+        y = line[1]
+        num_linhas += 1 
+
+        mse += ((theta_0 + theta_1 * x) - y)**2 
+
+    mse /= num_linhas
+
+    return mse
+
+    
 
 def step_gradient(theta_0, theta_1, data, alpha):
-    """
-    Executa uma atualização por descida do gradiente  e retorna os valores atualizados de theta_0 e theta_1.
-    :param theta_0: float - intercepto da reta
-    :param theta_1: float -inclinacao da reta
-    :param data: np.array - matriz com o conjunto de dados, x na coluna 0 e y na coluna 1
-    :param alpha: float - taxa de aprendizado (a.k.a. tamanho do passo)
-    :return: float,float - os novos valores de theta_0 e theta_1, respectivamente
-    """
-    raise NotImplementedError  # substituir pelo seu codigo
+    somatorio_t0 = 0
+    somatorio_t1 = 0
+    num_linhas = 0
+
+    for line in data:
+        x = line[0]
+        y = line[1]
+        num_linhas += 1 
+
+        somatorio_t0 += (theta_0 + theta_1 * x) - y
+        somatorio_t1 += ((theta_0 + theta_1 * x) - y) * x
+
+    derivada_t0 = (2/num_linhas) * somatorio_t0
+    derivada_t1 = (2/num_linhas) * somatorio_t1
+
+    novo_t0 = theta_0 - alpha * derivada_t0
+    novo_t1 = theta_1 - alpha * derivada_t1
+    
+    return (novo_t0, novo_t1)
+
 
 
 def fit(data, theta_0, theta_1, alpha, num_iterations):
-    """
-    Para cada época/iteração, executa uma atualização por descida de
-    gradiente e registra os valores atualizados de theta_0 e theta_1.
-    Ao final, retorna duas listas, uma com os theta_0 e outra com os theta_1
-    obtidos ao longo da execução (o último valor das listas deve
-    corresponder à última época/iteração).
+    list_theta0 = list()
+    list_theta1 = list()
+    list_aux = list()
 
-    :param data: np.array - matriz com o conjunto de dados, x na coluna 0 e y na coluna 1
-    :param theta_0: float - intercepto da reta
-    :param theta_1: float -inclinacao da reta
-    :param alpha: float - taxa de aprendizado (a.k.a. tamanho do passo)
-    :param num_iterations: int - numero de épocas/iterações para executar a descida de gradiente
-    :return: list,list - uma lista com os theta_0 e outra com os theta_1 obtidos ao longo da execução
-    """
-    raise NotImplementedError  # substituir pelo seu codigo
+    list_aux.append((theta_0, theta_1))
+    list_theta0.append(list_aux[-1][0])
+    list_theta1.append(list_aux[-1][1])
+
+    for i in range(num_iterations):
+        list_aux.append(step_gradient((list_aux[-1])[0], (list_aux[-1])[1], data, alpha))
+        list_theta0.append(list_aux[-1][0])
+        list_theta1.append(list_aux[-1][1])
+
+    return list_theta0, list_theta1
